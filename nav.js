639bc243c8697ea0ae2601a19e63fc2b88421cbf
karoljaw <karoljaworek04@gmail.com>
2018-01-16 18:28:43 +0100

    Add files via upload

--- nav.js
+++ nav.js
@@ -1,9 +1,24 @@
 var toggleul = document.getElementById("toggle-nav");
 var toggleicon = document.querySelector(".nav-burger");
+let navbar = document.querySelector(".nav");
+let body = document.querySelector("body");
+
+
+let lis = document.getElementsByTagName("li");
+lis = Array.from(lis);
+
 
 toggleicon.addEventListener("click", function(){
     toggleul.classList.toggle("nav-opened");
 });
+
+lis.forEach(function(li){
+    li.addEventListener("click", function(){
+        toggleul.classList.toggle("nav-opened");
+    })
+})
+
+
 
 
   
