b624403d7b1420833a0e8122ce5342ee42be9460
Karol Jaworek <karoljaworekpoczta@gmail.com>
2018-01-16 16:46:54 +0100

    final version

---
+++ nav.js
@@ -0,0 +1,10 @@
+var toggleul = document.getElementById("toggle-nav");
+var toggleicon = document.querySelector(".nav-burger");
+
+toggleicon.addEventListener("click", function(){
+    toggleul.classList.toggle("nav-opened");
+});
+
+
+  
+
