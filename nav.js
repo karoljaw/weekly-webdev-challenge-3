344260beff9eb5257f4a414cb9c7a25c7801533f
karoljaw <karoljaworek04@gmail.com>
2018-01-17 14:12:14 +0100

    Add files via upload

--- nav.js
+++ nav.js
@@ -1,22 +1,26 @@
-var toggleul = document.getElementById("toggle-nav");
-var toggleicon = document.querySelector(".nav-burger");
-let navbar = document.querySelector(".nav");
-let body = document.querySelector("body");
+const toggleul = document.getElementById("toggle-nav");
+const toggleicon = document.querySelector(".nav-burger");
+let as = document.querySelectorAll("a");
+as = Array.from(as);
 
 
-let lis = document.getElementsByTagName("li");
-lis = Array.from(lis);
+toggleicon.addEventListener("click", toggleMenu);
 
-
-toggleicon.addEventListener("click", function(){
-    toggleul.classList.toggle("nav-opened");
+as.forEach(function(e) {
+    e.addEventListener("click", closeMenu); 
 });
 
-lis.forEach(function(li){
-    li.addEventListener("click", function(){
+
+function toggleMenu(e) {
         toggleul.classList.toggle("nav-opened");
-    })
-})
+}
+
+function closeMenu(e) {
+    if (e.target.parentElement.parentElement.classList.contains("nav-opened")) {
+        toggleul.classList.toggle("nav-opened");
+    }
+}
+
 
 
 
